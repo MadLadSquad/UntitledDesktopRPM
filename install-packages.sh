01e4ec9a4e02324b77cd34c428c7fc1ebd766945
Stanislav Vasilev <stanv.contact176@gmail.com>
2025-08-23 20:05:14 +0300

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -7,15 +7,15 @@ install_package() {
   dnf install -y "$RPM"
 }
 
-install_package UntitledCLIParser
-install_package UntitledDBusUtils
-install_package UntitledExec
-install_package UntitledI18N
-install_package UntitledOpen
-install_package UntitledXDGBasedir
+install_package UntitledCLIParser untitled-cli-parser
+install_package UntitledDBusUtils untitled-dbus-utils
+install_package UntitledExec untitled-exec
+install_package UntitledI18N untitled-i18n
+install_package UntitledOpen untitled-open
+install_package UntitledXDGBasedir untitled-xdg-basedir
 
-install_package UntitledImGuiFramework
+install_package UntitledImGuiFramework untitled-imgui-framework
 
-install_package UntitledDESessionLogout
-install_package UntitledGameSystemManager
-install_package UntitledIBusHandwriting
+install_package UntitledDESessionLogout ude-session-logout
+install_package UntitledGameSystemManager untitled-game-system-manager
+install_package UntitledIBusHandwriting untitled-ibus-handwriting
